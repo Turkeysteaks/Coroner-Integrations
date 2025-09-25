cb1fb5f7c5a8852ea936967e9d394c2c52baaf12
Turkeysteaks <rogansmith33@gmail.com>
2025-09-25 16:14:28 +0100

    Missing files and better build script

--- Releases/build.sh
+++ Releases/build.sh
@@ -2,7 +2,15 @@ cp ../Art/icon.png .
 cp ../Art/manifest.json .
 cp ../README.md .
 cp ../CHANGELOG.md .
-cp ../CoronerSirenHead/build/bin/Debug/*.dll ./
+cp ../CoronerBiodiversity/build/bin/Debug/*.dll ./
+#Create the config directory if it doesn't already exist and copy the Strings_* files into it.
 mkdir -p ./BepInEx/config/EliteMasterEric-Coroner/ && cp ../LanguageData/* ./BepInEx/config/EliteMasterEric-Coroner/
 
-zip -r ./CoronerSirenHead.zip ./* -x ./build.*
+output="CoronerBiodiversity_v$(jq -r .version_number manifest.json).zip"
+echo "${output}"
+#Compress everything except for this file into a .zip
+zip -r ./${output} ./* -x ./build.* -x ./*.zip
+
+#jq -r .version_number manifest.json
+#CAREFUL. Deletes everything in the folder except for the new zip and this file.
+find ../Releases/* -not -name 'build.sh' -not -name ${output} -delete
